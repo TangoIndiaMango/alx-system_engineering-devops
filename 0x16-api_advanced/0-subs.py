bb114cb87b7470e835aa793479aea394b95fe2d3
TangoIndiaMango <aliyutimileyin2340@gmail.com>
2023-08-09 05:41:31 -0500

    api

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,12 @@
+"""This python module contains a script that interacts with the Reddit API"""
+from requests import get
+
+
+def number_of_subscribers(subreddit):
+    """Queries a reddit api and returns the number of subscribers for
+    a given subredit"""
+    header = {"User-Agent": "0x16.api.advanced.project:mondayodoh"}
+    res = get("https://www.reddit.com/r/{}/about.json".format(subreddit),
+              headers=header, allow_redirects=False)
+    code = res.status_code
+    return 0 if code == 404 else res.json().get("data").get("subscribers")
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+0x16 Api practice on reddit api
